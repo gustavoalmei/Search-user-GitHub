93df2709152627d037edacbef803f7543ae55557
Gustavo Almeida <73246160+gustavoalmei@users.noreply.github.com>
2021-10-26 23:06:41 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function getInforUserGitHub() {
+  const url = `https://api.github.com/users/${"gustavoalmei"}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if (data.login !== undefined) {
+        photoUser.src = data.avatar_url
+        nameUser.textContent = data.name
+        login.textContent = `@${data.login}`
+        login.href = data.html_url
+        bio.textContent = data.bio
+        followers.textContent = `${data.followers} followers`
+        following.textContent = `${data.following} following`
+        repository.textContent = `${data.public_repos} repository`
+        localization.textContent = data.location
+        twitter.textContent = data.twitter_username
+        twitterLink.href = `https://twitter.com/${data.twitter_username}`
+        blog.textContent = data.blog
+        blogLink.href = `https://${data.blog}/`
+        company.textContent = data.company
+      } else {
+        alert('Digite o nome de um usuário.')
+      }
+    })
+}
